b5c0b0d7fe932881c8821d304c3cfa7a74a8a22d
UmmuRasul <ummurasul5@gmail.com>
2020-03-19 08:48:33 +0300

    properties

--- home.js
+++ home.js
@@ -1,16 +1,10 @@
-let key = 42; //global scope
-function getSecretCode(value) {
-    let keyGenerator = function() {
-        let key = 12;
-        console.log('in keyGenerator', key);
-        return key; //finish executing key no longer exists
-    }
-    let code = value * keyGenerator();
-    console.log('in getSecretCode:', key); //use the key outside fn
-    return code;
-}
-let secretCode = getSecretCode(2)
-showMessage(secretCode);
-
-
-changePercentOff(30);
+let mySymbol = Symbol();
+let person = {
+    name: 'Mariam',
+    age: 40,
+    partTime: false,
+    [mySymbol]: 'secretInformation'
+};
+person.age = 49; //dot notation
+//person['age'] = 47; //sq bracket notation
+showMessage(person.age); //the dot syntax
