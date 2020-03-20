58c386ca6d38db6715ff5aaeb1ba2e60c0bc525d
UmmuRasul <ummurasul5@gmail.com>
2020-03-20 11:56:33 +0300

    added comments

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 function showMessage(message) {
     document.getElementById('message').textContent = message;
-}
+} //document is the build-in represent the webpage
+//textContent is the property
 
 function changePercentOff(percentage) {
     document.getElementById("percent-off").textContent = percentage + "% OFF";
