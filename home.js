25611bd190ca86a017c3767389a7865e71b835ff
UmmuRasul <ummurasul5@gmail.com>
2020-03-20 13:05:06 +0300

    toggling the div

--- home.js
+++ home.js
@@ -6,6 +6,14 @@
 const button = document.getElementById('sreveiw');
 
 button.addEventListener('click', function() {
-    console.log('click');
+    const review = document.getElementById('review');
 
+    //when clicked again to remove it we need conditioal stt
+    if (review.classList.contains('d-none')) {
+        review.classList.remove('d-none') //remove if element has d-none
+    } else {
+        review.classList.add('d-none') //add if does not contain d-none
+
+    }
+    // review.classList.remove('d-none'); // showing the hidden element by removing the d-none class by cling the button
 });
