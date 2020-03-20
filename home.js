5a2b666e0b1d2d2d713162ef7a5fef490282fb60
UmmuRasul <ummurasul5@gmail.com>
2020-03-20 11:37:45 +0300

    cheking on std built-in objects

--- home.js
+++ home.js
@@ -1,10 +1,8 @@
-let person = {
-    name: 'Mariam',
-    age: 40,
-    partTime: false,
-    showInfo: function(realAge) //method
-        {
-            showMessage(this.name + ' is   ' + realAge + ' years old'); //'this' refers to the current object
-        }
-};
-person.showInfo(45);
+// let now = new Date();
+// showMessage(now.toDateString());
+
+//showMessage(Math.abs(-34));
+//showMessage(Math.random());
+
+let a = 'Mariam';
+showMessage(a.charAt(2));
