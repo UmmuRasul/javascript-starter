5dc6de5792fefab89f0b67cfb08bdebd115ea5dd
UmmuRasul <ummurasul5@gmail.com>
2020-03-20 13:14:11 +0300

    changing the text on the button

--- home.js
+++ home.js
@@ -11,9 +11,11 @@ button.addEventListener('click', function() {
     //when clicked again to remove it we need conditioal stt
     if (review.classList.contains('d-none')) {
         review.classList.remove('d-none') //remove if element has d-none
+            //changing the text of the button when clicked
+        button.textContent = 'CLOSE REVIEW';
     } else {
         review.classList.add('d-none') //add if does not contain d-none
-
+        button.textContent = 'SEE REVIEW';
     }
     // review.classList.remove('d-none'); // showing the hidden element by removing the d-none class by cling the button
 });
