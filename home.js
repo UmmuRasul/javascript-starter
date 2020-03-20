c31d96fe6d079c065d5576e33dfcad73aa4b40bf
UmmuRasul <ummurasul5@gmail.com>
2020-03-20 12:21:48 +0300

    button see-review

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
-const header = document.getElementById('message');
-header.style.color = 'blue';
-header.style.fontWeight = '100';
+// const header = document.getElementById('message');
+// header.style.color = 'blue';
+// header.style.fontWeight = '100';
+
+
+const button = document.getElementById('see-review');
+
+button.addEventListener('click', function() {
+    console.log('click');
+
+});
