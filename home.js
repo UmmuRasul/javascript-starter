aca053848310f66f46bbd9fdf43368f2c7cb9cd0
UmmuRasul <ummurasul5@gmail.com>
2020-03-23 09:09:20 +0300

    Array searching and looping

--- home.js
+++ home.js
@@ -1,4 +1,29 @@
-const values = ['a', 'b', 'c', 'd', 'e'];
-values.splice(2, 2); //splice can delete
-values.splice(2, 0, 'Mariam'); //can add values
-console.log(values)
+//searching through an Array using indexOf
+const values = ['a', 'b', 'c', 'd', 'z'];
+console.log(values.indexOf('a'));
+console.log(values.indexOf('m')); //get -1 insted of undefine
+console.log(values.indexOf('c'));
+
+
+//filter thru an Array
+const values2 = ['a', 'b', 'c', 'd', 'z'];
+const set = values2.filter(function(item) {
+    console.log('item:', item);
+    return item > 'b';
+});
+console.log(set);
+
+//find ()
+const values3 = ['a', 'bbb', 'c'];
+
+const found = values3.find(function(item) {
+    return item.length > 1;
+});
+console.log(found);
+
+
+//forEach()
+const values4 = ['a', 'b', 'c', 'd', 'z'];
+values4.forEach(function(item) {
+    console.log(item)
+});
