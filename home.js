ea2ee2caa784987f909dc7d8d8ea2737214b6082
UmmuRasul <ummurasul5@gmail.com>
2020-03-24 11:07:18 +0300

    hoisting

--- home.js
+++ home.js
@@ -1,13 +1,8 @@
-//let productId = 3456; //we show to all functions
+// productId = 456;
+// console.log(productId);
+// let productId = 123;
 
+showProductId(); //hoisting
 function showProductId() {
-    let productId = 1234; //fix() also show this
-
-    function fix() { //executed first then the upper fn
-        let productId = 5678; //only fix function
-        console.log('in fix:', productId);
-    }
-    fix();
-    console.log('in showProduct:', productId);
+    console.log(580);
 }
-showProductId();
