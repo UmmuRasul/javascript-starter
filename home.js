2c6f767d0215eb3ac0e16f4bedfe32f855d106e1
UmmuRasul <ummurasul5@gmail.com>
2020-03-24 10:49:41 +0300

    globa scope

--- home.js
+++ home.js
@@ -1,4 +1,11 @@
-const containers =
-    document.getElementsByClassName('container');
-containers[0].classList.add('d-none'); //set the atribute to none by hiding
-console.log(containers)
+const app = {
+    productId: 12345,
+    userName: 'Mariam',
+    orderNum: 789
+};
+
+
+function showProductId() {
+    console.log(app.productId);
+}
+showProductId();
